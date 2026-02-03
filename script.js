c6d5e9a27ac9084c21f0a2b96ef1f53e278242bd
joliexs807 <8vcbqq25z5@privaterelay.appleid.com>
2026-02-03 05:46:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const buttons = document.querySelectorAll('.btn');
 
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    buttons.forEach(b => b.disabled = true);
+    btn.style.opacity = '1';
+  });
+});
